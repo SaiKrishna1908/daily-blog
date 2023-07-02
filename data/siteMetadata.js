669c03682098685252b290760b63c9281501627c
krishna <saikrishna1908@gmail.com>
2023-07-02 11:32:29 +0530

    changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Daily-Blog',
+  author: 'Sai Krishna',
+  headerTitle: 'Daily-Blog',
+  description: 'A blog for sharing my thoughts & findings',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -10,13 +10,14 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'saikrishna1908@gmail.com',
+  github: 'https://github.com/SaiKrishna1908',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/sai-krishna-3a581a152/',
   locale: 'en-US',
+  //TODO: Add google analytics
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
